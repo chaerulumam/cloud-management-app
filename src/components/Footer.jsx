846575246643a8a78abeadcab9b62e22a63813e5
Chaerul Umam <chaerulumamb244@gmail.com>
2022-10-09 22:49:49 +0700

    first commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,94 @@
+import React from "react";
+import {
+  FaFacebook,
+  FaGithub,
+  FaTwitter,
+  FaInstagram,
+  FaTiktok,
+} from "react-icons/fa";
+
+const Footer = () => {
+  return (
+    <div className="w-full mt-24 bg-slate-900 text-gray-300 py-y px-2">
+      <div className="max-w-[1240px] mx-auto grid sm:grid-cols-6 border-b-2 border-gray-600 py-2">
+        <div>
+          <h6 className="font-bold uppercase pt-2">Solutions</h6>
+          <ul>
+            <li className="py-1">Marketing</li>
+            <li className="py-1">Analytics</li>
+            <li className="py-1">Commerce</li>
+            <li className="py-1">Data</li>
+            <li className="py-1">Cloud</li>
+          </ul>
+        </div>
+        <div>
+          <h6 className="font-bold uppercase pt-2">Support</h6>
+          <ul>
+            <li className="py-1">Pricing</li>
+            <li className="py-1">Documentation</li>
+            <li className="py-1">Guides</li>
+            <li className="py-1">API Status</li>
+          </ul>
+        </div>
+        <div>
+          <h6 className="font-bold uppercase pt-2">Company</h6>
+          <ul>
+            <li className="py-1">About</li>
+            <li className="py-1">Career</li>
+            <li className="py-1">Blog</li>
+            <li className="py-1">Press</li>
+            <li className="py-1">Partner</li>
+          </ul>
+        </div>
+        <div>
+          <h6 className="font-bold uppercase pt-2">Legal</h6>
+          <ul>
+            <li className="py-1">Claims</li>
+            <li className="py-1">Privacy</li>
+            <li className="py-1">Terms</li>
+            <li className="py-1">Policies</li>
+            <li className="py-1">Conditions</li>
+          </ul>
+        </div>
+
+        <div className="col-span-2 pt-8 md:pt-2">
+          <p className="font-bold uppercase">Subscribe to our newsletter</p>
+          <p className="py-4">
+            The latest news, articles, and resources, sent to your inbox weekly.
+          </p>
+          <form className="flex flex-col sm:flex-row">
+            <input
+              className="w-full p-2 mr-4 rounded-md mb-4"
+              type="email"
+              placeholder="Enter your email.."
+            />
+            <button className="p-2 mb-4">Subscribe</button>
+          </form>
+        </div>
+      </div>
+
+      <div className="flex flex-col max-w-[1240px] px-2 py-4 mx-auto justify-between sm:flex-row text-center text-slate-400">
+        <p className="py-4">2022 JAMET, Inc. All right reserved</p>
+        <div className="flex justify-between sm:w-[300px] pt-4 text-2xl">
+          <a href="https://web.facebook.com/chaerulumamf/">
+            <FaFacebook />
+          </a>
+          <a href="#">
+            <FaTwitter />
+          </a>
+          <a href="#">
+            <FaInstagram />
+          </a>
+          <a href="#">
+            <FaTiktok />
+          </a>
+          <a href="https://github.com/chaerulumam">
+            <FaGithub />
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
